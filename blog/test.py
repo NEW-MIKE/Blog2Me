1939105ea21cef8ac6f0a80afed1f1f17cab9d27
NEW-MIKE <2454069901@qq.com>
2022-01-14 14:37:45 +0800

    init

--- blog/test.py
+++ blog/test.py
@@ -49,10 +49,10 @@ class RequestHandler(BaseHTTPRequestHandler):
             print("The directory is deleted successfully")
 
         shutil.move(srcdir, dstdir)
-        subprocess.Popen( ['python3', 'dev.py'], shell = True ).communicate()
+        subprocess.Popen( ['python3', 'dev.py'] ).communicate()
         print("启动新的进程")
         return
-    
+
 
     def handle_request_url_verify(self, post_obj):
         # 原样返回 challenge 字段内容
