6b6ca8782a7befa03d9089c130b5362b32de9ad7
NEW-MIKE <2454069901@qq.com>
2022-01-14 16:15:36 +0800

    init

--- blog/test.py
+++ blog/test.py
@@ -57,7 +57,7 @@ class RequestHandler(BaseHTTPRequestHandler):
             print("The directory is deleted successfully")
 
         shutil.move(srcdir, dstdir)
-        subprocess.Popen( ['python3', '/tmp/blog/blog/dev.py'] )
+        subprocess.Popen(['python3', '/tmp/blog/blog/dev.py'])
         os.chdir("/tmp/blog/blog")
         print("启动新的进程")
         return
