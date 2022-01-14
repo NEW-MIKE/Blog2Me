187c1c2d511961e6e8dffaa583f49f7ed0705b4b
NEW-MIKE <2454069901@qq.com>
2022-01-14 13:39:41 +0800

    init

--- blog/test.py
+++ blog/test.py
@@ -16,7 +16,6 @@ from git.repo import Repo
 srcdir = "/tmp/blog/book"
 dstdir = "/tmp/blog"
 
-
 APP_ID = "cli_a15bebebc5b8d00b"
 APP_SECRET = "pMJXu20Pn2L2fmFIvwSrZcPmZbRnmotd"
 APP_VERIFICATION_TOKEN = "hxaTTQZc9re73RE4bsKaEcCvLxLr1NIY"
