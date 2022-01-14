ddd4baa7c6033fe9d7c156cad5bcfd2db38e237f
NEW-MIKE <2454069901@qq.com>
2022-01-14 13:32:12 +0800

    init

--- blog/test.py
+++ blog/test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # --coding:utf-8--
 import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
+#reload(sys)
+#sys.setdefaultencoding('utf-8')
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from os import path
 import json
