e82d42260bbab5be608d50a1e7c03c74d522d30c
NEW-MIKE <2454069901@qq.com>
2022-01-14 14:31:51 +0800

    init

--- blog/test.py
+++ blog/test.py
@@ -31,36 +31,6 @@ def killport(port):
 	command='''kill -9 $(netstat -nlp | grep :'''+str(port)+''' | awk '{print $7}' | awk -F"/" '{ print $1 }')'''
 	os.system(command)
 
-def kill_port_process(port):
-    # 根据端口号杀死进程
-    
-    ret = os.popen("netstat -nao|findstr " + str(port))
-    str_list = ret.read()
-
-    if not str_list:
-        print('端口未使用')
-        return
-    # 只关闭处于LISTENING的端口
-    if 'TCP' in str_list:
-        ret_list = str_list.replace(' ', '')
-        ret_list = re.split('\n', ret_list)
-        listening_list = [rl.split('LISTENING') for rl in ret_list]
-        process_pids = [ll[1] for ll in listening_list if len(ll) >= 2]
-        process_pid_set = set(process_pids)
-        for process_pid in process_pid_set:
-            os.popen('taskkill /pid ' + str(process_pid) + ' /F')
-            print(port, '端口已被释放')
-            time.sleep(1)
-        
-    elif 'UDP' in str_list:
-        ret_list = re.split(' ', str_list)
-        process_pid = ret_list[-1].strip()
-        if process_pid:
-            os.popen('taskkill /pid ' + str(process_pid) + ' /F')
-            print('端口已被释放')
-        else:
-            print("端口未被使用")
-
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
