0f0bb76eee41d881d04403961935a2b32b2552d8
mk <2454069901@qq.com>
2022-02-11 19:36:20 +0800

    init

--- blog/dev.py
+++ blog/dev.py
@@ -55,6 +55,15 @@ class RequestHandler(BaseHTTPRequestHandler):
             self.wfile.write(f.read())
             print("css")
             f.close()
+        elif self.path.endswith(".js"):
+            print("enter css")
+            f = open(filepath[1:],"rb")
+            self.send_response(200)
+            self.send_header('Content-type', 'text/javascript')
+            self.end_headers()
+            self.wfile.write(f.read())
+            print("javascript")
+            f.close()
         else:
             f = open(filepath[1:],"rb")
             html = f.read()
@@ -62,130 +71,6 @@ class RequestHandler(BaseHTTPRequestHandler):
             f.close()
         #self.wfile.write(html.encode())
         return
-    def handle_request_url_verify(self, post_obj):
-        # 原样返回 challenge 字段内容
-        challenge = post_obj.get("challenge", "")
-        rsp = {'challenge': challenge}
-        self.response(json.dumps(rsp))
-        return
-
-    def handle_message(self, event):
-        # 此处只处理 text 类型消息，其他类型消息忽略
-        msg_type = event.get("msg_type", "")
-        if msg_type != "text":
-            print("unknown msg_type =", msg_type)
-            self.response("")
-            return
-
-        # 调用发消息 API 之前，先要获取 API 调用凭证：tenant_access_token
-        access_token = self.get_tenant_access_token()
-        if access_token == "":
-            self.response("")
-            return
-
-        # 机器人 echo 收到的消息
-        self.send_message(access_token, event.get("open_chat_id"), event.get("text"))
-        #self.response("")
-        
-        self.response(json.dumps(ret_card))
-        return
-
-    def response(self, body):
-        self.send_response(200)
-        self.send_header('Content-Type', 'application/json')
-        self.end_headers()
-        self.wfile.write(body.encode())
-
-    def get_tenant_access_token(self):
-        url = "https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal/"
-        headers = {
-            "Content-Type" : "application/json"
-        }
-        req_body = {
-            "app_id": APP_ID,
-            "app_secret": APP_SECRET
-        }
-
-        data = bytes(json.dumps(req_body), encoding='utf8')
-        req = request.Request(url=url, data=data, headers=headers, method='POST')
-        try:
-            response = request.urlopen(req)
-        except Exception as e:
-            print(e.read().decode())
-            return ""
-
-        rsp_body = response.read().decode('utf-8')
-        rsp_dict = json.loads(rsp_body)
-        code = rsp_dict.get("code", -1)
-        if code != 0:
-            print("get tenant_access_token error, code =", code)
-            return ""
-        return rsp_dict.get("tenant_access_token", "")
-
-    def send_message(self, token, open_id, text):
-        url = "https://open.feishu.cn/open-apis/message/v4/send/"
-
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer " + token
-        }
-        req_body = {
-            "chat_id": open_id,
-            "msg_type":"interactive",
-              "card":{
-                "header":{
-                  "title":{
-                    "tag":"plain_text",
-                    "content":"iPad Air 3"
-                  }
-                },
-                "elements":[
-                  {
-                    "tag":"img",
-                    "img_key":"img_2098a60d-8267-4d4c-91a8-d94b2baf90dg",
-                    "alt":{
-                      "tag":"plain_text",
-                      "content":"iPad Air 3"
-                    }
-                  },
-                  {
-                    "tag":"div",
-                    "text":{
-                      "tag":"lark_md",
-                      "content":"活动描述：**Apple 出品**\n开奖时间：**2019-12-11 18:00**"
-                    }
-                  },
-                  {
-                    "tag":"action",
-                    "actions":[
-                      {
-                        "tag":"button",
-                        "text":{
-                          "tag":"plain_text",
-                          "content":"参加抽奖"
-                        },
-                        "type":"default"
-                      }
-                    ]
-                  }
-                ]
-              }
-        }
-
-        data = bytes(json.dumps(req_body), encoding='utf8')
-        req = request.Request(url=url, data=data, headers=headers, method='POST')
-        try:
-            response = request.urlopen(req)
-        except Exception as e:
-            print(e.read().decode())
-            return
-
-        rsp_body = response.read().decode('utf-8')
-        rsp_dict = json.loads(rsp_body)
-        code = rsp_dict.get("code", -1)
-        if code != 0:
-            print("send message error, code = ", code, ", msg =", rsp_dict.get("msg", ""))
-
 def run():
     port = 4456
     server_address = ('', port)
