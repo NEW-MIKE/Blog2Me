a9ebe9e5fdd5fc79b0f941d013122a0eb0fcab1f
mk <2454069901@qq.com>
2022-02-25 14:00:19 +0800

    fix bug

--- blog/dev.py
+++ blog/dev.py
@@ -25,7 +25,8 @@ ret_card = {
 class RequestHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
-        
+        time = datetime.datetime.strptime(string,'%Y-%m-%d %H:%M:%S')
+        print(time)
         self.send_response(200)
         self.send_header('Content-Type', 'application/json')
         self.end_headers()
@@ -147,7 +148,6 @@ def create__file(file_path,msg):
     f.write(msg)
     f.close()
 def create__filea(file_path,msg):
-    #time = datetime.datetime.strptime(string,'%Y-%m-%d %H:%M:%S')
     f=open(file_path,"a",encoding='utf-8')
     f.write('<br>')
     f.write(msg)
