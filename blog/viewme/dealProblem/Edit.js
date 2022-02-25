d02774546927b0418f81ca96c3e80c556f3791b0
mk <2454069901@qq.com>
2022-02-25 13:29:44 +0800

    fix bug

--- blog/viewme/dealProblem/Edit.js
+++ blog/viewme/dealProblem/Edit.js
@@ -33,3 +33,25 @@ function uploaddata(){
     console.log(data)
     xhr.send(data);    
 }
+
+function downloaddata(){
+    console.log("ok")
+    var url = "https://9f01-115-192-20-43.ngrok.io/cv.txt";
+    var xhr = new XMLHttpRequest();
+
+    //2-设置请求报文
+    //2-1请求行
+    xhr.open('get', url);
+    //2-2请求头
+    xhr.setRequestHeader('content-type', 'text/html');
+    //2-3请求主体
+    xhr.send(null);
+
+    //3-监听服务器响应
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var r = xhr.responseText;          
+            document.querySelector('.info').innerHTML = r; //显示
+        }
+    }
+}
