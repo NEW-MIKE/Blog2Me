c7d9946464c8be753635b551c2788038f4290177
mk <2454069901@qq.com>
2022-02-25 14:38:52 +0800

    init

--- blog/viewme/dealProblem/Edit.js
+++ blog/viewme/dealProblem/Edit.js
@@ -29,7 +29,7 @@ function uploaddata(){
           console.log(xhr.responseText);
        }};
     console.log(document.getElementById("jilu").value)
-    var data = "中国"+document.getElementById("jilu").value;
+    var data = document.getElementById("jilu").value;
     console.log(data)
     xhr.send(data);    
 }
