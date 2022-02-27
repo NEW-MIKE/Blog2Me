1253249f6a4024603a1e4cb36be6618d4c76cb47
mk <2454069901@qq.com>
2022-02-27 10:39:42 +0800

    init

---
+++ blog/viewme/GodMovie/js/openweb.js
@@ -0,0 +1,9 @@
+function openwebclick(url){
+    if(url == "任阅"){
+        window.open("https://sourcegraph.com/github.com/smuyyh/BookReader/-/tree/app/src/main/java/com/justwayward/reader");  
+        console.log("任阅");
+    }
+    else{
+        console.log("unknow");
+    }
+}
