1143042c692bf261d9e6d5fa16d273590dcd4bae
mk <2454069901@qq.com>
2022-06-01 20:45:47 +0800

    d

--- blog/data.js
+++ blog/data.js
@@ -2,6 +2,6 @@ function getUrl(){
     var url; 
     url = window.location.hostname;
     console.log(123)
-    console.log(url);     
+    console.log("https://"+url);     
     return "https://0bd7-115-197-101-3.ngrok.io";    
 }
